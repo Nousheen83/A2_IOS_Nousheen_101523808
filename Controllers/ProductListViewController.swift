f3ba02bad572dd29d4573de43aabbdc8bada9c9c
Nousheen82 <10september83@gmail.com>
2026-04-09 23:14:02 +0500

    Final UI polish

--- Controllers/ProductListViewController.swift
+++ Controllers/ProductListViewController.swift
@@ -2,32 +2,34 @@ import UIKit
 import CoreData
 
 class ProductListViewController: UITableViewController {
-    
+
     var products: [Product] = []
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         fetch()
     }
-    
+
     func fetch() {
         let request: NSFetchRequest<Product> = Product.fetchRequest()
         products = try! CoreDataManager.shared.context.fetch(request)
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         products.count
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
-    -> UITableViewCell {
-        
+
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        
+
         let p = products[indexPath.row]
         cell.textLabel?.text = p.name
         cell.detailTextLabel?.text = p.desc
-        
+
         return cell
     }
 }
