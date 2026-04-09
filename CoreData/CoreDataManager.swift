72e48f3fbaff884816bb502d1e9b796bf3e79349
Nousheen82 <10september83@gmail.com>
2026-04-09 21:51:56 +0500

    Added Core data model

---
+++ CoreData/CoreDataManager.swift
@@ -0,0 +1,27 @@
+import UIKit
+import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ProductModel")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Core Data Failed: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func saveContext() {
+        if context.hasChanges {
+            try? context.save()
+        }
+    }
+}
