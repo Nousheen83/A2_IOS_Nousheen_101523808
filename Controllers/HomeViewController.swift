e323c41c34d31f8bf6d59a0aefef7c66c2b9c111
Nousheen82 <10september83@gmail.com>
2026-04-09 23:12:29 +0500

    Fixed bugs in nav

--- Controllers/HomeViewController.swift
+++ Controllers/HomeViewController.swift
@@ -2,27 +2,27 @@ import UIKit
 import CoreData
 
 class HomeViewController: UIViewController {
-    
+
     var products: [Product] = []
     var index = 0
-    
+
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var providerLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchProducts()
-        display()
+        showProduct()
     }
-    
+
     func fetchProducts() {
         let request: NSFetchRequest<Product> = Product.fetchRequest()
         products = try! CoreDataManager.shared.context.fetch(request)
     }
-    
-    func display() {
+
+    func showProduct() {
         if products.count > 0 {
             let p = products[index]
             nameLabel.text = p.name
@@ -31,18 +31,18 @@ class HomeViewController: UIViewController {
             providerLabel.text = p.provider
         }
     }
-    
+
     @IBAction func nextTapped(_ sender: UIButton) {
         if index < products.count - 1 {
             index += 1
-            display()
+            showProduct()
         }
     }
-    
+
     @IBAction func prevTapped(_ sender: UIButton) {
         if index > 0 {
             index -= 1
-            display()
+            showProduct()
         }
     }
 }
