8bce2f720dd49f872e60a61b59668079b65f478c
Nousheen82 <10september83@gmail.com>
2026-04-09 23:08:26 +0500

    Implemented table view

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,18 +1,38 @@
-func insertDummyData() {
-    let context = CoreDataManager.shared.context
-    
-    let request: NSFetchRequest<Product> = Product.fetchRequest()
-    let count = try! context.count(for: request)
-    
-    if count == 0 {
-        for i in 1...10 {
-            let p = Product(context: context)
-            p.productID = Int64(i)
-            p.name = "Product \(i)"
-            p.desc = "Description for product \(i)"
-            p.price = Double(i * 10)
-            p.provider = "Provider \(i)"
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        insertDummyData()
+        return true
+    }
+
+    // MARK: - Dummy Data (10 Products)
+    func insertDummyData() {
+        
+        let context = CoreDataManager.shared.context
+        let request: NSFetchRequest<Product> = Product.fetchRequest()
+        
+        let count = try! context.count(for: request)
+        
+        if count == 0 {
+            for i in 1...10 {
+                let p = Product(context: context)
+                p.productID = Int64(i)
+                p.name = "Product \(i)"
+                p.desc = "This is product number \(i)"
+                p.price = Double(i * 15)
+                p.provider = "Provider \(i)"
+            }
+            try? context.save()
         }
-        try? context.save()
     }
 }
